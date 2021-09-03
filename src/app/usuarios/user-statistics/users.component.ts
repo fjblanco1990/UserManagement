a18b9bb22cdbc72ed54554df68c3de9cdf58d3a0
fjblanco1990 <fjblanco1990@gmail.com>
2021-09-03 14:58:33 -0500

    Correcciones

--- src/app/usuarios/user-statistics/users.component.ts
+++ src/app/usuarios/user-statistics/users.component.ts
@@ -1,5 +1,5 @@
 import { GetUsers } from './../../store/actions/users-list.actions';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ChartDataSets, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
@@ -15,19 +15,9 @@ import { UserService } from '../../services/user.service';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit {
-  ingresoNum: number = 0;
-  egresoNum: number = 0;
-
-  totalIngresos: number = 0;
-  totalEgresos: number = 0;
-  modelArrayFollower: number[] = [];
+export class UsersComponent implements OnInit, OnDestroy {
+  modelArrayFollower: any[] = [];
   modelArrayName: string[] = [];
-
-  objetcArrary: {
-    followers: number[];
-    name: string[];
-  };
 
   estadisticaSubs!: Subscription;
 
@@ -38,36 +28,49 @@ export class UsersComponent implements OnInit {
   public barChartData: ChartDataSets[] = [];
   userDetail: UsersModel[] = [];
   error: any;
-  private subscriptions: Subscription;
+  private subscriptions = new Subscription();
   constructor(
     private store: Store<AppState>,
     private UserService: UserService
   ) {}
 
   ngOnInit(): void {
-    this.subscriptions = this.store
-      .select('UsersList')
-      .subscribe(({ users }) => {
-        this.generateGrapich(users);
-      });
-    this.store.dispatch(GetUsers());
+    this.UserService.getUsers().subscribe( (lstUser: any) => {
+      this.userDetail = lstUser;
+      this.generateGrapich(this.userDetail);
+    });
+  }
+
+  ngOnDestroy() {
+
   }
 
   generateGrapich(infoUser: UsersModel[]) {
     this.modelArrayFollower = [];
     this.modelArrayName = [];
     infoUser.forEach((element) => {
-      this.UserService.getUsersByFollower(element.login).subscribe((result: any) => {
-
+     this.UserService.getUsersByFollower(element.login).subscribe((result: any) => {
         this.modelArrayFollower.push(result.followers);
-        this.modelArrayName.push(result.login);
-
-        this.barChartData = [
-          { data: [this.modelArrayFollower, 0], label: 'Seguidores' },
-        ];
-
-        this.barChartLabels = this.modelArrayName;
-      });
+        this.modelArrayName.push(result.login);       
+      })
     });
+    this.barChartData = [
+      // 65, 59, 80, 81, 56, 55, 40
+
+      // 0: 22649
+      // 1: 21266
+      // 2: 8215
+      // 3: 9845
+      // 4: 511 
+      // 5: 124
+      // 6: 1399
+      // 7: 153
+      // this.modelArrayFollower
+
+      // 22649,21266,8215 ,9845,511, 124,1399,153
+      { data: [this.modelArrayFollower], label: 'Seguidores' },
+    ];
+    this.barChartLabels = this.modelArrayName;
+    
   }
 }
